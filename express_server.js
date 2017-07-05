20a8858d07f9598e8e7e3a45bae609f97f3eec94
Marcos Silva <marcosgustavobh@gmail.com>
2017-07-05 18:15:43 +0000

    added Delete and Edit functions

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,15 +10,16 @@ app.set("view engine", "ejs");
 // URL Database
 let urlDatabase = {
   "b2xVn2": {
-    shortURL: "b2xVn2",
-    longURL: "http://www.lighthouselabs.ca" }
+    shortUrl: "b2xVn2",
+    longUrl: "http://www.lighthouselabs.ca"
+  },
+
+  "9sm5xK": {
+    shortUrl: "9sm5xK",
+    longUrl: "http://www.google.com"
+  }
 }
 
-app.get("/u/:shortURL", (req, res) => {
-  let shortURL = urlDatabase.b2xVn2.shortURL;
-  let longURL = urlDatabase.b2xVn2.longURL;
-  res.redirect(longURL);
-});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -28,16 +29,43 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+//Generates a new random short URL, adds it to the urlDatabase and redirects to URL's index page
+app.post("/urls", (req, res) => {
+  let shortUrl = generateRandomString()
+  let longUrl = req.body.longUrl
+  urlDatabase[shortUrl] = {
+    shortUrl: shortUrl,
+    longUrl: longUrl,
+  }
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString()
-  let longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL
+//Goes to the longUrl page
+app.get("/u/:shortUrl", (req, res) => {
+  let shortUrl = req.params.shortUrl;
+  let longUrl = urlDatabase[shortUrl].longUrl;
+  res.redirect(longUrl)
+});
+
+//Deletes an Url
+app.post("/urls/:id/delete", (req, res) => {
+  let shortUrl = req.params.id;
+  delete urlDatabase[shortUrl];
   res.redirect("/urls");
+});
+
+//Updates an Url
+app.post("/urls/:id", (req, res) => {
+  let shortUrl = req.params.id;
+  let longUrl = req.body.longUrl
+  urlDatabase[shortUrl].longUrl = longUrl
+  res.redirect(`/urls/${shortUrl}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -45,7 +73,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let shortUrl = req.params.id
+  let longUrl = urlDatabase[shortUrl].longUrl
+  let templateVars = {
+    shortUrl: shortUrl,
+    longUrl: longUrl
+  }
   res.render("urls_show", templateVars);
 });
 
@@ -56,10 +89,10 @@ app.listen(PORT, () => {
 
 // Generates a random string
 function generateRandomString() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var length = 6;
-  var randomString = "";
-  for (var i = length; i > 0; --i) {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let length = 6;
+  let randomString = "";
+  for (let i = length; i > 0; --i) {
     randomString += chars[Math.floor(Math.random() * chars.length)];
   }
   return randomString;
